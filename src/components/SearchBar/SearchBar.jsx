acfe3fc64d7a0390d5346c0de956eb93a289197b
CausevicAmila <amila.causevic64@gmail.com>
2023-05-29 23:33:37 +0200

    search bar update

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './SearchBar.css';
-import { FaSearch } from 'react-icons/fa';
+import { GoSearch } from 'react-icons/go';
 
 const SearchBar = () => {
 
   return (
     <>
       <input type="text" className="search-input" placeholder="Search..." />
-      <button className="search-button">
-        <FaSearch className="search-icon" />
+      <button className="search-button font-sans">
+        <GoSearch className="search-icon " />
         Search
       </button>
     </>
