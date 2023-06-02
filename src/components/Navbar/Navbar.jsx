dcca824277f0217028ae0fa9ff4b7bd07c00bbc8
CausevicAmila <amila.causevic64@gmail.com>
2023-06-02 13:31:56 +0200

    new navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,71 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Logo from "../../public/balogo.png";
-import NavLinks from "./NavLinks";
+import { Component } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { MenuItems } from './MenuItems';
 
-const Navbar = () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <nav className="bg-lightgray border-b border-gray-200">
-      <div className="flex items-center font-semibold font-inter text-primary text-2xl leading-8 ">
-        <div className="z-50 p-5 md:w-auto w-full flex justify-between">
-        <a href="/">
-          <img src={Logo} alt="logo" className="md:cursor-pointer h-15 m-5" />
+
+class Navbar extends Component{
+  state = {clicked: false};
+  handleClick = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+  render(){
+    return (
+      <nav className='NavbarItems bg-lightgray text-primary font-semibold font-inter text-2xl leading-8 uppercase '>
+      <div className='logo'>
+          <a href="/">
+            <img src="/src/public/balogo.png" alt="logo" />
           </a>
-          <div className="md:hidden font-semibold font-inter text-primary text-3xl mt-8" onClick={() => setOpen(!open)}>
-            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
-          </div>
-        </div>
-        <ul className="md:flex hidden items-center font-semibold font-inter text-primary text-2xl leading-8 mt-10 gap-3">
-        <NavLinks />
-          <li>
-            <Link to="/aboutus" className="py-7 px-3 inline-block">
-              ABOUT US
-            </Link>
-          </li>
 
-          <li>
-            <Link to="/form" className="py-7 px-3 inline-block">
-              SELL
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="py-7 px-3 inline-block">
-              LOG IN
-            </Link>
-          </li>
-        </ul>    
-        
-        {/* Mobile nav */}
-        <ul
-          className={`
-        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-28 pl-4
-        duration-500 ${open ? "left-0" : "left-[-100%]"}
-        `}
-        >
-        <NavLinks />
-          <li>
-            <Link to="/aboutus" className="py-7 px-3 inline-block">
-              ABOUT US
-            </Link>
-          </li>
-          <li>
-            <Link to="/sellform" className="py-7 px-3 inline-block">
-              SELL
-            </Link>
-          </li>
-          <li>
-          <Link to="/login" className="py-7 px-3 inline-block">
-            LOG IN 
-          </Link>
-        </li>
-          <div className="py-5">
-            
-          </div>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+        </div>
+        <div className='menu-icons  text-primary font-semibold font-inter text-2xl leading-8' onClick={this.handleClick}> 
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      
+        </div>
+          <ul className={this.state.clicked ? "nav-menu active text-primary font-semibold font-inter text-2xl leading-8  uppercase" : "nav-menu"} >
+            {MenuItems.map((item, index) => {
+              return(
+                <li key={index} id = 'nav-link'>
+                  <Link>
+                  {item.title}
+                  </Link>
+                </li>
+              )
+            })}
+          
+          </ul>
+      </nav>
+    )
+  }
+} 
 
 export default Navbar;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, BrowserRouter as Routes } from "react-router-dom";
-import Sales from './routes/Sales'
 import SearchBar from "./components/SearchBar/SearchBar";
-function App() {
+function Home() {
   return (
     <section className="h-screen">
         <Navbar />
-  
+        <div className="search-bar"> 
+           <SearchBar/>
+        </div>
+        <div className="image">
+          <img src="../src/public/home.png" alt="" />
+        </div>
         
     </section>
     
@@ -15,4 +18,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
