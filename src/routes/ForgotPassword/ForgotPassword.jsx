11adcb9834fa3facddb8a567d130e3209e1bc13b
CausevicAmila <amila.causevic64@gmail.com>
2023-06-04 01:48:41 +0200

    forgot password done

--- src/routes/ForgotPassword/ForgotPassword.jsx
+++ src/routes/ForgotPassword/ForgotPassword.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
-import '../Login/Login.css'; // Import the CSS file for styling
+import '../ForgotPassword/ForgotPassword.css';
+import { MdEmail, MdPassword } from 'react-icons/md'; 
+import { Link } from 'react-router-dom';
 function ForgotPassword() {
     return(
-        <>
-        <div className="page-wrapper">
-          <Navbar />
-          <section className="h-screen">
-              <div className="h-containerlog bg-lightgray text-primary">
-                <div>
-                  <p className='text1  text-3xl leading-9 font-extrabold'>Lets’s sign you in!</p>
-                  <p className='text2 text-xl leading-7 font-normal' >Welcome back! <br></br><br></br>
-    
-                  Please enter your email address and password to access your account. </p>
-                </div>
+      <div className="page-wrapper">
+      <Navbar />
+      <section className="h-screen">
+          <div className="h-containerlog bg-lightgray text-primary">
+            <div>
+              <p className='text1  text-3xl leading-9 font-extrabold'>Forgot password?</p>
+              <p className='text2 text-xl leading-7 font-normal' >If you don't remember your password, please enter the email address. <br></br><br></br>
+
+              Subsequently, an email will be sent to your provided address containing a link to initiate the process of resetting your access code. </p>
+            </div>
+            <div className='form text-xl leading-7 font-normal text-primary'>
+              <div className='femail mt-16'>
+                <p >Email</p>
+                  <div className="email bg-lightgray">
+                    <MdEmail className="email-icon text-gray" />
+                    <input type="text" className="email-input" placeholder="Enter your email " />
+                  </div>
               </div>
-          </section>
-          <Footer /> 
-        </div>
-        </>
+
+            </div>
+            <div>
+              <button className="login-button bg-accent text-lightgray">Log in</button>
+            </div>
+          </div>
+      </section>
+      <Footer /> 
+    </div>
     );
   
 
